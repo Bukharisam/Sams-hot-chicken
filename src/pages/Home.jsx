13e4ae6acb33a76b4482f66cd2cbbcc7c664bb3f
Sameer Bukhari <Bukharisameer3@gmail.com>
2026-02-04 01:07:28 -0500

    started to implenent CSS into React Pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,22 @@
+import './Home.css';
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+    <nav>
+      <div className="left-nav">
+      <a href="/"><img className="logo-style" src="/images/logo.jpeg" alt="Sams Hot Chicken Logo" width="130" height="130" /></a>
+      <h1> Sam's Hot Chicken </h1>
+      </div>
+
+      <a href="/menu">Menu</a> |
+      <a href="/reviews">Reviews</a> |
+      <a href="/checkout">Checkout</a> |
+      <a href="/reviews">Reviews</a>
+      <button className='order-now-style'> Order Now </button>
+    </nav>
+      <h1>Home Page</h1>
+    </>
+  );
 }
+
